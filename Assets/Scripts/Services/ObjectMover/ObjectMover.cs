b395ac28331b73eb3eccfa85a83cb28f056f7f99
Serhiy Serhiychuk <34813681+serhiyhelp@users.noreply.github.com>
2023-09-06 21:00:41 +0300

    Insignificant Changes

--- Assets/Scripts/Services/ObjectMover/ObjectMover.cs
+++ Assets/Scripts/Services/ObjectMover/ObjectMover.cs
@@ -4,24 +4,24 @@ namespace Services.ObjectMover
 {
     public class ObjectMover : IObjectMover
     {
-        private float speed = 10f;
-        private bool isMoveAvailable;
+        private float _speed = 10f;
+        private bool  _isMoveAvailable;
 
         public void UpdateObjectPosition(Transform t, Vector3 direction)
         {
-            if(!isMoveAvailable) return;
+            if(!_isMoveAvailable) return;
         
-            t.Translate(direction * speed * Time.deltaTime);
+            t.Translate(direction * (_speed * Time.deltaTime));
         }
 
         public void MoveAction(bool isAvailable)
         {
-            isMoveAvailable = isAvailable;
+            _isMoveAvailable = isAvailable;
         }
 
         public void MoveTowardsToObject(Transform t, Vector3 target)
         {
-            t.position = Vector3.MoveTowards(t.position, target, speed * Time.deltaTime);
+            t.position = Vector3.MoveTowards(t.position, target, _speed * Time.deltaTime);
         }
     }
 }
